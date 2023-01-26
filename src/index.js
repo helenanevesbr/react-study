a992c64e32582516bffaf931b1078826862ca2f7
helena <helenaneves.br@gmail.com>
2023-01-26 09:43:06 -0300

    Armazenando um histórico de jogadas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+/* Vamos armazenar os arrays squares anteriores em um outro array chamado history. O array history representa todos os estados do tabuleiro, da primeira à última jogada,
+
+Colocar o state history no componente Game, nos permite remover o state squares de seu componente filho, Board.
+
+Isso permite que Game instrua o Board a renderizar turnos anteriores a partir do history.
+*/
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -11,53 +18,25 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    /* modificamos a função handleClick do Board para ignorar o click, caso alguém tenha vencido o jogo ou se o quadrado (square) já esteja ocupado */
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+
+        /*O componente Board receba as props squares e onClick do componente Game
+        Passamos a localização de cada Square para o manipulador onClick para indicar qual Square foi clicado */
       />
     );
   }
 
   render() {
-
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    /* Chamamos calculateWinner (função global) para checar se um jogador venceu. Caso tenha vencido, podemos mostrar um texto como “Winner: X” ou “Winner: O” */
-
     return (
       <div>
-        <div className="status">{status}</div>
+
+        {/*Uma vez que o componente Game agora está renderizando o status do jogo, nós podemos remover o código correspondente do método render do componente Board. */}
+
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -79,14 +58,60 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  /* configuramos o state inicial do componente Board para o componente Game em seu construtor*/
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    /*No componente Game, dentro do método handleClick, nós concatenamos novas entradas do histórico de jogadas em history. */
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    /* A função render do componente Game utiliza a entrada mais recente do histórico (history) para determinar e exibir o status do jogo. */
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
@@ -94,7 +119,6 @@ class Game extends React.Component {
   }
 }
 
-// A função abaixo irá verificar se há um vencedor e retornará 'X', 'O' ou null conforme apropriado.
 
 function calculateWinner(squares) {
 
@@ -108,42 +132,19 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  /*Aqui temos todas as combinações que caracterizam uma vitória no jogo.
-  No jogo da velha, temos 3 linhas, cada linha com 3 quadrados:
-    0  1  2
-    3  4  5
-    6  7  8
-  Uma forma de ganhar é se o valor (ou X ou O) contido no quadrado 0 for igual ao valor contido nos quadrados adjancentes, 1 e 2. Ou seja [0,1,2].
-  Outras combinações vencedoras são [0,3,6] e [0,4,8] que são, respectivamente, os quadrados da primeira coluna e os quadrados de uma das diagonais.
-  */
 
-  for (let i = 0; i < lines.length; i++) { //para cada combinação vencedora...
-    const [a, b, c] = lines[i]; //... temos constantes que guardam as posições dessa combinação
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
     
     if (
-      squares[a] && squares[a] === //para evitar que code quebre  caso square[a] esteja undefined
+      squares[a] && squares[a] ===
       
-      squares[b] && squares[a] === squares[c]) { //O conteúdo (ou X ou O) do quadrado em uma posição vencedora é igual ao contéudo dos quadrados nas outras 2 posições da combinação que resulta em vitória? Se sim...
-        return squares[a]; //retorne qual é esse conteúdo: ou X ou O.
+      squares[b] && squares[a] === squares[c]) {
+        return squares[a];
     }
   }
   return null;
 }
-
-/* Ordem de squares:
-
-  1° - Construído como uma array de 9 elementos nulos - Constructor do componente Board
-
-  2° - Antes mesmo de qualquer click, uma função para renderizar os quadrados é chamada: função render chama a função renderSquare - Componente Board
-  A função render organiza os 9 quadrados visualmente em 3 <div>, 1 <div> para cada linha.
-  Ela chama a renderSquare 9 vezes. Ao chamar, atribui i ao argumento da função, sendo i a posição do quadrado no tabulheiro. Ex: 1° quadrado -> renderSquare(0). Último quadrado -> renderSquare(8)
-
-  3° - O método renderSquare do Board passa um prop chamado value para o componente-função Square.
-  A props value do Square recebe o estado atual do quadrado
-    value={this.state.squares[i]}
-  squares no caso vai se referir a array ou do Constructor (caso nenhum clique tenha ocorrido ainda) ou a array gerada em handleClick.
-  O Square recebe esse parâmetro (aka prop) e retorna uma hierarquia de elementos para exibir através do método render.
-*/
 
 // ========================================
 
