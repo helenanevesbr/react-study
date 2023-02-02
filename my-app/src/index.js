47ccee69c91bea4c532b15e981cd8a5ce32ec1c1
helena <helenaneves.br@gmail.com>
2023-02-02 12:10:49 -0300

    Adding style with stylesheet

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting(){
-    return <div>
-        <Person/>
-        <Message/>
-        {/*Nest components*/}
-    </div>
-}
+import './index.css';
 
-const Person = () => <h2>john doe</h2>; //implicit return is possible
-const Message = () => { //however if you use brackets, you need explicit return
-    return <p>this is my message</p>
+const BookList = () => {
+    return (
+        <section className='booklist'>
+            {/*In React, you don't have class, you just have className. It's important to remember that while adding css to your application */}
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+};
+
+const Book = () => {
+    return (
+        <article className='book'>
+            <Image/>
+            <Title/>
+            <Author/>
+        </article>
+    );
+};
+
+const Image = () => <h2><img src="./images/lessonsinchesmitry.jpg" alt='Lessons in Chemistry: A Novel'/></h2>;
+const Title = () => <h2>Lessons in Chemistry: A Novel</h2>;
+const Author = () => {
+    return <h4>Bonnie Garmus</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting/>)
+root.render(<BookList/>)
