06588015b5ee210952ee0535c64e119c59447b3c
helena <helenaneves.br@gmail.com>
2023-02-02 12:33:29 -0300

    Adding in-line CSS with JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+// css rules still apply (inline vs external css)
 
 const BookList = () => {
     return (
         <section className='booklist'>
-            {/*In React, you don't have class, you just have className. It's important to remember that while adding css to your application */}
             <Book />
             <Book />
             <Book />
@@ -25,10 +25,22 @@ const Book = () => {
     );
 };
 
-const Image = () => <h2><img src="./images/lessonsinchesmitry.jpg" alt='Lessons in Chemistry: A Novel'/></h2>;
+const Image = () => <h2>
+    <img
+    src="./images/lessonsinchesmitry.jpg" 
+    alt='Lessons in Chemistry: A Novel'/>
+</h2>;
+
 const Title = () => <h2>Lessons in Chemistry: A Novel</h2>;
+
 const Author = () => {
-    return <h4>Bonnie Garmus</h4>;
+    return (
+        <h4
+        style={{color:'#617d98', fontSize: '0.75rem', marginTop: '0.5rem'}}>
+            {/*{} in JSX means going back to JS Land. That's how we add styles using JSX instead of external stylesheets (CSS). That's also how we wrote this comment*/}
+            Bonnie Garmus
+        </h4>
+    );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
