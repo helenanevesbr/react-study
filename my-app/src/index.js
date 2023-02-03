8541d1d985e78313fbcc750fa05683473fd06d3d
helena <helenaneves.br@gmail.com>
2023-02-03 00:36:08 -0300

    Refactoring with right props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,29 +7,21 @@ const title = "Lessons in Chemistry: A Novel"
 const author = "Bonnie Garmus"
 const img = './images/lessonsinchesmitry.jpg'
 
-const BookList = () => {
+function BookList() {
     return (
-        <section className='booklist'>
-
-            <Book job='developer' />
-            <Book title='random title' number={22} />
-
-            {/*props works simmilar to arguments, parameters and function.
-            In that analogy, the root component, BookList is the function calling passing arguments, and Book component is the function recieving those arguments.*/}
-
-        </section>
+      <section className='booklist'>
+        <Book author={author} title={title} img={img} />
+        <Book title={title} img={img} />
+      </section>
     );
-};
+}
 
 const Book = (props) => {
     return (
       <article className='book'>
-        <img src={img} alt={title} />
-        <h2>{title}</h2>
-        <h4>{author}</h4>
-        <p>{props.job}</p> {/* props works like an object. Props is the object, job is the property, developer is the value */}
-        <p>{props.title}</p> {/* First Book component doesn't have a title prop. If the prop exists, it will return value, otherwise no value. */}
-        <p>{props.number}</p>
+        <img src={props.img} alt={props.title} />
+        <h2>{props.title}</h2>
+        <h4>{props.author} </h4>
       </article>
     );
 };
