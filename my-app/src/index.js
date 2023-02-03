e9e94dd52141dfb194d8b8b561ff43a10c9d1361
helena <helenaneves.br@gmail.com>
2023-02-03 10:50:54 -0300

    Children prop

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -22,7 +22,17 @@ function BookList() {
         <Book
           author={firstBook.author}
           title={firstBook.title}
-          img={firstBook.img} />
+          img={firstBook.img}
+        >
+
+          <button>buy me</button>
+          {/* Children Prop is everything we render between component tags.
+          We mostly use it Context API, but here we are using it so we can render something (a "buy me" button) in one Book component, but not the other.
+          Prop is an object. When we add a children prop like this, automatically a property called "children" is added to the object, and it's value is equal to "<button>buy m..." in our case
+          To render the children prop, we have to access it... */}
+          
+        </Book>
+
         <Book
           author={secondBook.author}
           title={secondBook.title}
@@ -32,15 +42,13 @@ function BookList() {
     );
 }
 
-const Book = ({ img, title, author }) => {   /* Another way to destructure the props object to save time/typing is to destructure it inside function parameters */
-
-  // const { img, title, author } = props; --- this was the 2nd approach to destructure the object: we did it inside the component.
-
+const Book = ({ img, title, author, children}) => { //... like we access every other property of prop.
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
+      {children}
     </article>
   );
 };
