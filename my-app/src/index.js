c8d4cf889049f905c0893422da87ace5de6a045a
helena <helenaneves.br@gmail.com>
2023-02-03 00:12:40 -0300

    Adding javascript to JSX and refactoring index to only 2 components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-// css rules still apply (inline vs external css)
 
 const BookList = () => {
     return (
@@ -16,40 +15,30 @@ const BookList = () => {
 };
 
 const Book = () => {
+
+    const title = "Lessons in Chemistry: A Novel"
+    const author = "Bonnie Garmus"
+
     return (
-        <article className='book'>
-            <Image/>
-            <Title/>
-            <Author/>
-        </article>
+      <article className='book'>
+        <img
+          src='./images/lessonsinchesmitry.jpg'
+          alt='Lessons in Chemistry: A Novel'
+        />
+        <h2>{title}</h2>
+        <h4>{author.toUpperCase()}</h4>
+
+        {/* value inside must be an expression (return value), can't be a statement.
+        For instance, if we try to declare a variable inside like this <p>{let x = 6}</p>, it will throw an error
+        But <p>{6+6}, because 6+6 returns a value
+        Im a nutsheel, it behaves simmilarly to console.log</p>,
+        */}
+
+      </article>
     );
 };
 
-const Image = () => <h2>
-    <img
-    src="./images/lessonsinchesmitry.jpg" 
-    alt='Lessons in Chemistry: A Novel'/>
-</h2>;
-
-const Title = () => <h2>Lessons in Chemistry: A Novel</h2>;
-
-const Author = () => {
-
-    const inlineHeadingStyles = {
-        color: '#617d98',
-        fontSize: '0.75rem',
-        marginTop: '0.5rem',
-      };
-    {/*The styles we added at component Author are, essentially, key/value pairs  of an object. Therefore, we an add them as an object apart from the h4 element
-    */}
-
-    return (
-        <h4
-        style={inlineHeadingStyles}>
-            Bonnie Garmus
-        </h4>
-    );
-};
+/* refactor to single book component (personal preference) */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
