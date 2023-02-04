0be6b05699db43d14cc923a51dc9a9695e55fbff
helena <helenaneves.br@gmail.com>
2023-02-04 15:05:08 -0300

    Rendering object - listing the books part 1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,52 +3,50 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const firstBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting Facts For Curious Minds',
-  img: './images/interestingfactsforthecuriousmind.jpg',
-};
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/interestingfactsforthecuriousmind.jpg',
+  },
+  
+  {
+    author: "Bonnie Garmus",
+    title: "Lessons in Chemistry: A Novel",
+    img: './images/lessonsinchesmitry.jpg',
+  }
 
-const secondBook = {
-  title: "Lessons in Chemistry: A Novel",
-  author: "Bonnie Garmus",
-  img: './images/lessonsinchesmitry.jpg',
-}
+]
 
 function BookList() {
     return (
       <section className='booklist'>
 
-        <Book
-          author={firstBook.author}
-          title={firstBook.title}
-          img={firstBook.img}
-        >
+      {/* {books}
+      </section>
 
-          <button>buy me</button>
-          {/* Children Prop is everything we render between component tags.
-          We mostly use it Context API, but here we are using it so we can render something (a "buy me" button) in one Book component, but not the other.
-          Prop is an object. When we add a children prop like this, automatically a property called "children" is added to the object, and it's value is equal to "<button>buy m..." in our case
-          To render the children prop, we have to access it... */}
-          
-        </Book>
+      React can't render objects like books directly - this would thrown an error.
+      */}
 
-        <Book
-          author={secondBook.author}
-          title={secondBook.title}
-          img={secondBook.img} />
+        {books.map((book)=>{
+          return <div><h2>{book.title}</h2></div>
+        })}
+        {/*
+        The map function creates a new array from calling a function for every array element. Therefore, it creates a callback function for every item in the array.
+        It will iterate over each object of the array books. That's how we will pull out the properties of these objects.
+        Not only can React render an object (indirectly) like this, but it can also wrap the properties of these objects on a HTML structure - div, h1, etc).
+        */}
 
       </section>
     );
 }
 
-const Book = ({ img, title, author, children}) => { //... like we access every other property of prop.
+const Book = ({ img, title, author}) => {
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
-      {children}
     </article>
   );
 };
