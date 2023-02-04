4d23fe1627810e005feefa71899902e092c6165e
helena <helenaneves.br@gmail.com>
2023-02-04 17:15:17 -0300

    Key Prop

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,41 +3,41 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-/* Even though we can nicely return HTML elements directly in our callback function, the book component already has the HTML structure that we need. To render it, the component is simply looking for the props: img, title and author.*/
-
 const books = [
   {
     author: 'Jordan Moore',
     title: 'Interesting Facts For Curious Minds',
     img: './images/interestingfactsforthecuriousmind.jpg',
+    id: 1,
   },
   
   {
     author: "Bonnie Garmus",
     title: "Lessons in Chemistry: A Novel",
     img: './images/lessonsinchesmitry.jpg',
+    id: 2,
   }
 
 ]
+
+/* every time you render a list of items in React, React wants to keep track of those items using a Key prop.
+Basically it's the item's id, a value unique to the item that you can find inside the array and it's passed as a prop to the component which will render the item.*/
 
 function BookList() {
   return (
     <section className='booklist'>
 
       {books.map((book)=>{
-
-        const {img, title, author} = book; /* Destructured book */
-
+        console.log(book);
+        const {img, title, author, id} = book;
         return <Book
-
           img={img}
           title={title}
           author={author}
-          /*So I'm rendering now the Book component and passing the properties of object book one by one as props.
-          In a future refactoring, I'll pass the entire object, or I can spread out the properties. But for now, let's just go one by one. */
-
-        />
+          key={id} //Set the key prop on the main return, so it's passed down to the Book component
+        />;
       })}
+
     </section>
   );
 }
