64fe238427c5698dd6340a78d4b9a9505c11334b
helena <helenaneves.br@gmail.com>
2023-02-06 23:44:34 -0300

    ES6 modules: exporting Book component and booklist into index file

---
+++ my-app/src/books.js
@@ -0,0 +1,15 @@
+export const books = [ //named export - it's a type of export statement
+    {
+      author: 'Jordan Moore',
+      title: 'Interesting Facts For Curious Minds',
+      img: './images/interestingfactsforthecuriousmind.jpg',
+      id: 1,
+    },
+    
+    {
+      author: "Bonnie Garmus",
+      title: "Lessons in Chemistry: A Novel",
+      img: './images/lessonsinchesmitry.jpg',
+      id: 2,
+    }
+]
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,32 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './images/interestingfactsforthecuriousmind.jpg',
-    id: 1,
-  },
-  
-  {
-    author: "Bonnie Garmus",
-    title: "Lessons in Chemistry: A Novel",
-    img: './images/lessonsinchesmitry.jpg',
-    id: 2,
-  }
+import { books } from './books';
+/*named export - This export statement is useful when the imported file has more than one component.
+Names MUST match - that means ~import { *name* }~ must be the same as ~export *name*~
+*/
 
-]
+import Book from './Book';
+/*with default exports, the exported component can be renamed inside the imported file.
+However, there can be only one component per file */
 
 function BookList() {
-
-  const getBook = (id) => {
-    const book = books.find(
-      (book) => book.id === id
-    );
-    console.log(book);
-  }
-
   return (
     <section className='booklist'>
       {books.map((book)=>{
@@ -36,38 +20,12 @@ function BookList() {
         return <Book
           {...book}
           key={book.id}
-          getBook={getBook}
         />;
 
       })}
     </section>
   );
 }
-
-const Book = ({img, title, author, id, getBook}) => {
-
-  /*const getSingleBook = () =>{
-    getBook(id);
-  }
-  // One way to *reference* in Book the function getBook from BookList instead of *invoking* it is to set up a wraper function like getSingleBook...
-  */
-
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-
-      {/*<button onClick={getSingleBook}>Get book title</button>
-      // ... and reference it here
-      */}
-
-      <button onClick={() => getBook(id)}>Get book title</button>
-      {/* Another way is to wrap the getBook function in an anonymous arrow function, which is essentially the same as the other solution, except it is more elegant*/}
-
-      <h4>{author} </h4>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
