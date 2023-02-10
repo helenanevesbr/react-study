36c93128bf0e6031412e66a9fa98d01d3ba895ae
helena <helenaneves.br@gmail.com>
2023-02-10 20:53:57 -0300

    Setup components Part 2 - Setup components with default export

--- backroads-app-refactoring/backroads-app/src/App.js
+++ backroads-app-refactoring/backroads-app/src/App.js
@@ -1,19 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+
 function App() {
-
   return <>
-
-
-
-
-
-
-
-
-
-
-
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
   </>;
-
+  //import and render all components in App.js
 }
 
 export default App;
