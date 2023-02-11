f2e49364c6b0c2ca2261b92a5fbc5c617b96c7ce
helena <helenaneves.br@gmail.com>
2023-02-11 17:40:29 -0300

    Tours - Refactoring hard-code to dynamic data

--- backroads-app-refactoring/backroads-app/src/data.js
+++ backroads-app-refactoring/backroads-app/src/data.js
@@ -1,3 +1,10 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
+/* Instead of that static path which worked in HTML. Now we have dynamic input, like we did in Navbar.js */
+
 export const pageLinks = [
     { id: 1, href: '#home', text: 'home' },
     { id: 2, href: '#about', text: 'about' },
@@ -27,4 +34,47 @@ export const services = [
         icon: 'fas fa-socks fa-fw',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia'
     },
+]
+
+export const tours = [
+    {
+        id: 1,
+        title: "Tibet Adventure",
+        location: "China",
+        duration: 6,
+        price: 2100,
+        date: "August 26th, 2020",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        img: tour1,
+    },
+    {
+        id: 2,
+        title: "Best Of Java",
+        location: "Indonesia",
+        duration: 11,
+        price: 1400,
+        date: "October 1th, 2020",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        img: tour2,
+    },
+    {
+        id: 3,
+        title: "Explore Hong Kong",
+        location: "Hong Kong",
+        duration: 8,
+        price: 5000,
+        date: "September 15th, 2020",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        img: tour3,
+    },
+    {
+        id: 4,
+        title: "Kenya Highlights",
+        location: "Kenya",
+        duration: 20,
+        price: 3300,
+        date: "December 5th, 2019",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+        img: tour4,
+    }
 ]
