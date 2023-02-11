6dd0a7634d08ae27babad09de9f05eff3eb7b1a5
helena <helenaneves.br@gmail.com>
2023-02-11 11:57:02 -0300

    Hero and About - Fixing image import

--- backroads-app-refactoring/backroads-app/src/components/Navbar.js
+++ backroads-app-refactoring/backroads-app/src/components/Navbar.js
@@ -6,14 +6,15 @@ const Navbar = () => {
   return(
     <nav className="navbar">
       <div className="nav-center">
+
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
         </div>
-        <ul className="nav-links" id="nav-links">
 
+        <ul className="nav-links" id="nav-links">
           {pageLinks.map((link)=>{
             return (
               <li key={link.id}>
@@ -21,11 +22,9 @@ const Navbar = () => {
               </li>
             )
           })}
-
         </ul>
 
         <ul className="nav-icons">
-
           {socialLinks.map((link)=>{
             return (
               <li key={link.id}>
@@ -38,19 +37,8 @@ const Navbar = () => {
               </li>
             )
           })}
-
-          {/*<li>
-            <a href="https://www.facebook.com" target="_blank" className="nav-icon">
-            
-              <i className="fab fa-facebook"></i>
-              /* We've linked font-awesome library in "public/index.html"
-              The way font awesome icons work is that you don't need social media (facebook, twitter) icons saved in your app, for instance in your images folder. 
-              You just write the className as shown above
-
-            </a>
-          </li>*/}
-
         </ul>
+        
       </div>
     </nav>
   );
