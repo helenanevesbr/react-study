34dc65fd55646253605103af07813243158612c6
helena <helenaneves.br@gmail.com>
2023-02-11 18:58:16 -0300

    Footer - re-se page and social links

--- backroads-app-refactoring/backroads-app/src/components/Footer.js
+++ backroads-app-refactoring/backroads-app/src/components/Footer.js
@@ -1,40 +1,48 @@
+import {pageLinks, socialLinks} from '../data'
+
 const Footer = () => {
   return(
     <footer className="section footer">
-      <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">home</a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">about</a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">services</a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">featured</a>
-        </li>
+
+      <ul className="footer-links"
+      /*{...pageLinks.map((link)=>{
+          return(
+            const {id, href, text} = link
+            key={id}
+      I was tempted to not close <ul> yet, wrap *key* inside map while also keeping it as a attribute for ul, outside of map.
+      It threw error "key is undefined"
+      ul is the parent element for every li list item, but li is one level above all the values to be rendered for that particular item*/
+      >
+
+        {pageLinks.map((link)=>{
+          const {id, href, text} = link
+          return(
+
+            <li key={id}>
+            {/* */}
+
+              <a href={href} className="footer-link">{text}</a>
+            </li>
+          )
+        })}
       </ul>
+
       <ul className="footer-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-facebook"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-twitter"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-squarespace"></i
-          ></a>
-        </li>
+        {socialLinks.map((link)=>{
+          const {id, href, icon} = link
+          return(
+            <li key={id}>
+              <a href={href} target="_blank" className="footer-icon"
+                ><i className={icon}></i
+              ></a>
+            </li>
+          )
+        })}
       </ul>
+
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
   );
