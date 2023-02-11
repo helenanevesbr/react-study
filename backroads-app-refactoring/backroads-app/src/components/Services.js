40405b370d840575731fd087409c91dca6b715b7
helena <helenaneves.br@gmail.com>
2023-02-11 17:50:22 -0300

    Tours - Forgot to delete some code and comments

--- backroads-app-refactoring/backroads-app/src/components/Services.js
+++ backroads-app-refactoring/backroads-app/src/components/Services.js
@@ -11,17 +11,12 @@ const Services = () => {
 
         {services.map((service)=>{
           return(
-
             <article className="service" key={service.id}>
-            {/* always add key attribute to the parent React element.
-            Article is one level above the elements which will render the props*/}
-
               <span className="service-icon"><i className={service.icon}></i></span>
               <div className="service-info">
                 <h4 className="service-title">{service.title}</h4>
                 <p className="service-text">{service.text}</p>
               </div>
-
             </article>
           )
         })}
--- backroads-app-refactoring/backroads-app/src/components/Tours.js
+++ backroads-app-refactoring/backroads-app/src/components/Tours.js
@@ -32,27 +32,6 @@ const Tours = () => {
             </article>
           )
         })}
-
-        <article className="tour-card">
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Tibet Adventure</h4>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-              vitae tempore voluptatum maxime reprehenderit eum quod
-              exercitationem fugit, qui corporis.
-            </p>
-            <div className="tour-footer">
-              <p>
-                <span><i className="fas fa-map"></i></span> china
-              </p>
-              <p>6 days</p>
-              <p>from $2100</p>
-            </div>
-          </div>
-        </article>
-
       </div>
     </section>
   );
