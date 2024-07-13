619bb6e783d5361f6fb2969f6c0ba6676ab59acf
Helena Neves <helenaneves.br@gmail.com>
2024-07-13 17:41:07 -0300

    refactor(sistema): Deleta feat desnecessaria

--- playing_around_with_react_official_documentation/src/App.mjs
+++ playing_around_with_react_official_documentation/src/App.mjs
@@ -47,26 +47,3 @@ function MyButton({count, onClick}){
   );
 }
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
-
-  return (
-    <ul>{listItems}</ul>
-  );
-}
-
--- playing_around_with_react_official_documentation/src/index.mjs
+++ playing_around_with_react_official_documentation/src/index.mjs
@@ -10,7 +10,6 @@ root.render(
   <StrictMode>
     <Profile />
     <MyButtons />
-    <ShoppingList />
     <ProductTable />
   </StrictMode>
 );
